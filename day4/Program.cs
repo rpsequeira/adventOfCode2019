55807b0a2fcfab1efae9baf95d9026b5eea577f6
rpsequeira <ricardoseq@gmail.com>
2019-12-07 12:19:20 +0000

    day4ish

--- day4/Program.cs
+++ day4/Program.cs
@@ -9,18 +9,106 @@ namespace adventofcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 3 of AoC");
-            string input = args[0];
-            Console.WriteLine($"Loading data from " + input + "...");
-            var lines = Tools.Tools.ReadAllLines(input).ToList();
-            Console.WriteLine("STEP 1");            
-            //var res = GetOptimalPath(lines);
-            //Console.WriteLine("Result: '" + res + "'");
+            Console.WriteLine("Day 4 of AoC");
+            var lines = "402328-864247".Split("-"); ;
+            Console.WriteLine("STEP 1");
+            var res = GetPossiblePasswordsNumber(lines.First(), lines.Last());
+            Console.WriteLine("Result: '" + res + "'");
             Console.WriteLine("STEP 2");
-            var res2 = GetShortestPath(lines);
-            Console.WriteLine("Result: '" + res2 + "'");
+            //Console.WriteLine("Result: '" + res2 + "'");
             Console.WriteLine("END");
         }
+
+        private static int GetPossiblePasswordsNumber(string start, string end)
+        {
+            Console.WriteLine(start + " - " + end);
+            List<int> first = start.Select(s => Int32.Parse(s.ToString())).ToList();
+            List<int> last = end.Select(s => Int32.Parse(s.ToString())).ToList();
+            List<int> password = new List<int>(first);
+            int endNumber = Int32.Parse(end);
+            int res = 0;
+            for (int i = first[0]; i <= last[0]; i++)
+            {
+                password[0] = i;
+                for (int j = 0; j <= 9; j++)
+                {
+                    password[1] = j;
+                    for (int k = 0; k <= 9; k++)
+                    {
+                        password[2] = k;
+                        for (int l = 0; l <= 9; l++)
+                        {
+                            password[3] = l;
+                            for (int m = 0; m <= 9; m++)
+                            {
+                                password[4] = m;
+                                for (int n = 0; n <= 9; n++)
+                                {
+                                    password[5] = n;
+                                    int a = Int32.Parse(string.Join("", password.Select(s => s.ToString())));
+                                    if (a > endNumber)
+                                    {
+                                        continue;
+                                    }
+                                    if (a < endNumber)
+                                    {
+                                        continue;
+                                    }
+                                    if (ValidatePassword(password))
+                                    {
+                                        res++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+
+        private static bool ValidatePassword(List<int> password)
+        {
+            var pairs = 0;
+            var pairPossible = false;
+            var lastPair = -99;
+            for (int i = 0; i < password.Count(); i++)
+            {
+                var lastDigit = -1;
+                if (password[i] < lastDigit)
+                {
+                    return false;
+                }
+                if (password[i] == lastDigit)
+                {
+                    if (pairPossible)
+                    {
+                        pairPossible = false;
+                        pairs++;
+                        lastPair = password[i];
+                    }
+                    else
+                    {
+                        pairPossible = true;
+                    }
+                }
+                else
+                {
+                    if (pairPossible)
+                    {
+                        if (lastPair == lastDigit)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                lastDigit = password[i];
+            }
+            return true;
+        }
+
+
 
         private static int GetShortestPath(List<string> lines)
         {
