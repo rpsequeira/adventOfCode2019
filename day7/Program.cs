c2f765c7afa03a43cab882da961d565a07f70c58
Ricardo Sequeira <rpsequeira@Ricardos-MacBook-Air.local>
2019-12-09 23:41:52 +0000

    lost..

--- day7/Program.cs
+++ day7/Program.cs
@@ -9,108 +9,19 @@ namespace adventofcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 4 of AoC");
-            var lines = "402328-864247".Split("-"); ;
+            Console.WriteLine("Day 7 of AoC");
+            string input = args[0];
+            Console.WriteLine($"Loading data from " + input + "...");
+            var lines = Tools.Tools.ReadAllLines(input).ToList();
             Console.WriteLine("STEP 1");
-            var res = GetPossiblePasswordsNumber(lines.First(), lines.Last());
-            var a = new[] { 7, 8, 8, 8, 9, 9 }.ToList();
-            var res2 = ValidatePassword(a);
-            Console.WriteLine("Result: '" + res + "'");
-            Console.WriteLine("Result: '" + res2 + "'");
+            //var res = GetOptimalPath(lines);
+            //var res = GetOptimalPath(lines);
+            //Console.WriteLine("Result: '" + res + "'");
             Console.WriteLine("STEP 2");
-            //Console.WriteLine("Result: '" + res2 + "'");
+            var res2 = GetShortestPath(lines);
+            Console.WriteLine("Result: '" + res2 + "'");
             Console.WriteLine("END");
         }
-
-        private static int GetPossiblePasswordsNumber(string start, string end)
-        {
-            Console.WriteLine(start + " - " + end);
-            List<int> first = start.Select(s => Int32.Parse(s.ToString())).ToList();
-            List<int> last = end.Select(s => Int32.Parse(s.ToString())).ToList();
-            List<int> password = new List<int>(first);
-            int startNumber = Int32.Parse(start);
-            int endNumber = Int32.Parse(end);
-            int res = 0;
-
-            for (int i = first[0]; i < last[0]; i++)
-            {
-                password[0] = i;
-                for (int j = 0; j <= 9; j++)
-                {
-                    password[1] = j;
-                    for (int k = j; k <= 9; k++)
-                    {
-                        password[2] = k;
-                        for (int l = k; l <= 9; l++)
-                        {
-                            password[3] = l;
-                            for (int m = l; m <= 9; m++)
-                            {
-                                password[4] = m;
-                                for (int n = m; n <= 9; n++)
-                                {
-                                    password[5] = n;
-                                    int a = Int32.Parse(string.Join("", password.Select(s => s.ToString())));
-                                    if (a > endNumber || a < startNumber)
-                                    {
-                                        continue;
-                                    }
-                                    if (ValidatePassword(password))
-                                    {
-                                        res++;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return res;
-        }
-
-
-        private static bool ValidatePassword(List<int> password)
-        {
-            var repeatedDigits = 0;
-            var lastDigit = -1;
-            var pairs = 0;
-            for (int i = 0; i < password.Count(); i++)
-            {
-                repeatedDigits++;
-                if (password[i] < lastDigit)
-                {
-                    return false;
-                }
-
-                if (password[i] != lastDigit && lastDigit > 0)
-                {
-                    // if (pairs > 1 && repeatedDigits % 2 != 0)
-                    // {
-                    //     return false;
-                    // }
-                    // else
-                    // {
-                    if (repeatedDigits == 2)
-                    {
-                        pairs++;
-                        repeatedDigits = 1;
-                    }
-                    // }
-                }
-                lastDigit = password[i];
-            }
-            // if (repeatedDigits > 1 && repeatedDigits % 2 != 0)
-            // {
-            //     return false;
-            // }
-            if (pairs > 0)
-            {
-                return false;
-            }
-            return true;
-        }
-
-
 
         private static int GetShortestPath(List<string> lines)
         {
---
+++ day8/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Tools;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 8 of AoC");
+            string input = args[0];
+            Console.WriteLine($"Loading data from " + input + "...");
+            var lines = Tools.Tools.ReadAllLines(input).First();
+            Console.WriteLine("STEP 1");
+            var layers = GetLayers(6, 25, lines);
+            // var layers = GetLayers(2, 2, "0222112222120000");
+            var layer = GetLayerWithLessErrors(layers);
+            var res = GetNumber(layer);
+            Console.WriteLine("Result: '" + res + "'");
+            Console.WriteLine("STEP 2");
+            var res2 = GetFinalImage(layers);
+            // Console.WriteLine("Result: '" + res2 + "'");
+            PrintImage(res2, 25, 6);
+            Console.WriteLine("");
+            Console.WriteLine("END");
+        }
+
+        private static List<List<int>> GetLayers(int height, int width, string input)
+        {
+            var res = new List<List<int>>();
+            var totalCharsPerLayer = height * width;
+            var numberOfLayers = input.Length / totalCharsPerLayer;
+
+            for (int i = 0; i < numberOfLayers; i++)
+            {
+                var layer = new List<int>();
+                for (int j = 0; j < totalCharsPerLayer; j++)
+                {
+                    var pos = i * totalCharsPerLayer + j;
+                    layer.Add(Int32.Parse(input[pos].ToString()));
+                }
+                res.Add(layer);
+            }
+            return res;
+        }
+
+        private static List<int> GetLayerWithLessErrors(List<List<int>> layers)
+        {
+            int res = 0;
+            int counter = 9000;
+            for (int i = 0; i < layers.Count(); i++)
+            {
+                var tempCounter = 0;
+                foreach (var pixel in layers[i])
+                {
+                    if (pixel == 0)
+                    {
+                        tempCounter++;
+                    }
+                }
+                if (tempCounter < counter)
+                {
+                    res = i;
+                    counter = tempCounter;
+                }
+            }
+            return layers[res];
+        }
+
+        private static int GetNumber(List<int> layer)
+        {
+            int onesCounter = 0;
+            int twosCounter = 0;
+
+            foreach (var item in layer)
+            {
+                if (item == 1)
+                {
+                    onesCounter++;
+                }
+                if (item == 2)
+                {
+                    twosCounter++;
+                }
+            }
+
+            return onesCounter * twosCounter;
+        }
+
+        private static List<int> GetFinalImage(List<List<int>> layers)
+        {
+            var res = new List<int>(layers.First());
+            // for (int i = 1; i < layers.Count(); i++)
+            // {
+            //     for (int j = 0; j < layers[i].Count(); j++)
+            //     {
+            //         if (res[j] == 2)
+            //         {
+            //             res[j] = layers[i][j];
+            //         }
+            //     }
+            // }
+
+            for (int i = 0; i < res.Count(); i++)
+            {
+                for (int j = 0; j < layers.Count(); j++)
+                {
+                    if (layers[j][i] != 2)
+                    {
+                        res[i] = layers[j][i];
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+
+        private static void PrintImage(List<int> image, int width, int height)
+        {
+            for (int i = 0; i < image.Count; i++)
+            {
+                if (i > 0 && i % width == 0)
+                {
+                    Console.WriteLine("");
+                }
+                if (image[i] == 1)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }   
+            }
+        }
+    }
+}
